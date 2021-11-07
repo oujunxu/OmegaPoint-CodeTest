e4c72e6de0aea8ba78472343c48fc38219da8c2b
oujunxu <oujun96@gmail.com>
2021-11-07 11:17:21 +0100

    -Added model classes for all the products. -Enabled CORS in Startup.

---
+++ Data/Model/ProductModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OmegaPointSimpleAPI.Data.Model
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public string Desciption { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+        public RatingModel Rating { get; set; }
+    }
+}
---
+++ Data/Model/RatingModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OmegaPointSimpleAPI.Data.Model
+{
+    public class RatingModel
+    {
+        public float Rate { get; set; }
+        public int Count { get; set; }
+    }
+}
